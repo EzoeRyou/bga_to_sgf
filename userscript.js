ac33736f03bb85dc6cc8f5d23c6396b1e2f739bf
Ryou Ezoe <boostcpp@gmail.com>
2020-06-14 16:29:38 +0900

    polish

--- userscript.js
+++ userscript.js
@@ -1,24 +1,25 @@
-// This script let you generate GNU Backgammon File from Boardgamearena.com's Backgammon log.
+// This script let you generate GNU Backgammon File(SGF) from Boardgamearena.com's Backgammon log.
+// Execute this JavaScript at the replay page where the URL is boardgamearena.com/gamereview?table=id
 // You must modify the your_name variable to your account's display name
 
 
 
 // Since BGA log shows from your perspective, the script must need to know which player is you.
-let your_name = "ezoe" ;
+// modify this variable to your account's display name.
+let your_name = "your name" ;
 
+// fixed header.
+let sgf_header = "(;FF[4]GM[6]CA[UTF-8]AP[GNU Backgammon:1.06.002]" ;
 
 // grab necessary DOM elements.
 let gamelogs = document.getElementById("gamelogs") ;
 let logs = gamelogs.childNodes ;
 
-// fixed header.
-let sgf_header = "(;FF[4]GM[6]CA[UTF-8]AP[GNU Backgammon:1.06.002]" ;
-
 // player names
 let players = document.getElementById("game_result").getElementsByClassName("playername") ;
 
-// retrieve the player names.
-let first_next_player = 0
+// retrieve the player names
+let first_next_player = 0 ;
 while( logs[first_next_player].textContent !== "Next player" )
     ++first_next_player ;
 let first_player = logs[first_next_player+1].textContent.match(/(.+) rolls/)[1] ;
@@ -30,12 +31,13 @@ let first_color = is_your_1st_player ? "B" : "W" ;
 let second_color = is_your_1st_player ? "W" : "B" ;
 
 let sgf_names = "PW[" + ( is_your_1st_player ? second_player : first_player ) + "]PB[" + your_name + "]" ;
+
 // nocube rule
 let sgf_rule = "RU[NoCube]\n" ;
 
 
 
-// true means white player, false means black player
+// true means first player, false means second player
 // fliped every time it see "Next player"
 // the initial value is the opposite because it will be fliped at first.
 let current_player = false ;
@@ -53,6 +55,15 @@ function translate_point( point )
         return 25 - point ;
 }
 
+// GNU Backgammon represent point 1-24 as a-x, bar as y, bear off as z.
+// This helper function convert string 1-24 to string a-x while converting to number and calculate the code point.
+function point_mapper( point_number_string )
+{
+    let point_number = translate_point( parseInt( point_number_string ) ) - 1 ;
+    let point_alphabet_charcode = "a".charCodeAt(0) + point_number ;
+    let point_alphabet = String.fromCharCode( point_alphabet_charcode ) ;
+    return point_alphabet ;
+}
 
 // buffer for accumulating the log
 let sgf_buffer = "" ;
@@ -76,6 +87,7 @@ for ( let i = 0 ; i !== logs.length ; ++i )
         sgf_buffer += ";" + (current_player ? first_color : second_color ) + "[" ;
         continue ;
     }
+
     // roll the dice
     let roll_result = value.textContent.match(/rolls (\d+)-(\d+)/) ;
     if ( roll_result !== null )
@@ -84,15 +96,6 @@ for ( let i = 0 ; i !== logs.length ; ++i )
         continue ;
     }
 
-    // GNU Backgammon represent point 1-24 as a-x, bar as y, bear off as z.
-    // This helper function convert string 1-24 to string a-x while converting to number and calculate the code point.
-    function point_mapper( point_number_string )
-    {
-        let point_number = translate_point( parseInt( point_number_string ) ) - 1 ;
-        let point_alphabet_charcode = "a".charCodeAt(0) + point_number ;
-        let point_alphabet = String.fromCharCode( point_alphabet_charcode ) ;
-        return point_alphabet ;
-    }
     // move the stones
     let move_result = value.textContent.match(/moves from point (\d+) to point (\d+)/) ;
     if ( move_result !== null )
@@ -100,6 +103,7 @@ for ( let i = 0 ; i !== logs.length ; ++i )
         sgf_buffer += point_mapper( move_result[1] ) + point_mapper( move_result[2] ) ;
         continue ;
     }
+
     // moves from the bar
     let move_from_bar_result = value.textContent.match(/moves from the bar to point (\d+)/) ;
     if ( move_from_bar_result !== null )
@@ -118,13 +122,13 @@ for ( let i = 0 ; i !== logs.length ; ++i )
     }
     // if none of the above branch handle the element, it can be ignored.    
 }
-// end the turn
+// end the turn and SGF file.
 sgf_buffer += "])" ;
-// construct the full GNU Backgammon File.
+
+// construct the full GNU Backgammon File(SGF format).
 sgf_text = sgf_header + sgf_names + sgf_rule + sgf_buffer ;
 
 // display it.
-
 let textarea = document.createElement("textarea") ;
 textarea.value = sgf_text ; 
 document.body.insertBefore( textarea, document.body.firstChild ) ;
